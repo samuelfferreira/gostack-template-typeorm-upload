4c75d591ea3699bb63fbe9da79e0bbf543b984c0
Samuel F Ferreira <samuel_ferreira@outlook.com>
2020-05-08 08:37:29 -0300

    Desafio 06 GoStack

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,4 +1,4 @@
-import { EntityRepository, Repository } from 'typeorm';
+import {EntityRepository, Repository} from 'typeorm';
 
 import Transaction from '../models/Transaction';
 
@@ -11,7 +11,51 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactions = await this.find();
+
+    const {income, outcome} = transactions.reduce((accumulator, transaction) => {
+      switch (transaction.type) {
+        case "income":
+          accumulator.income += Number(transaction.value);
+          break;
+        case "outcome":
+          accumulator.outcome += Number(transaction.value);
+          break;
+        default:
+          break;
+      }
+      return accumulator;
+    }, {
+      income: 0,
+      outcome: 0,
+      total: 0
+    })
+
+    const total = income - outcome;
+
+    return {income, outcome, total}
+
+
+    // const transactions = await this.find();
+    //
+    // const income = transactions.reduce((incomeTotal, transiction) => {
+    //   return (
+    //     incomeTotal + (transiction.type === 'income' ? transiction.value : 0)
+    //   );
+    // }, 0);
+    // const outcome = transactions.reduce((outcomeTotal, transiction) => {
+    //   return (
+    //     outcomeTotal + (transiction.type === 'outcome' ? transiction.value : 0)
+    //   );
+    // }, 0);
+    //
+    // const balance = {
+    //   income,
+    //   outcome,
+    //   total: income - outcome,
+    // };
+    //
+    // return balance;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,73 @@
-// import AppError from '../errors/AppError';
+import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
+import {getCustomRepository, getRepository} from "typeorm";
+import TransactionsRepository from "../repositories/TransactionsRepository";
+import Category from "../models/Category";
+
+interface RequestDto {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({title, value, type, category}: RequestDto): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const categoryRepository = getRepository(Category);
+
+    const {total} = await transactionsRepository.getBalance();
+
+    if (type === 'outcome' && total < value) {
+      throw new AppError(
+        'Saldo insuficiente'
+      );
+    }
+
+    let categoryExist = await categoryRepository.findOne({
+      where:
+        {
+          title: category
+        }
+    });
+
+    if (!categoryExist) {
+      categoryExist = categoryRepository.create({
+        title: category
+      });
+      await categoryRepository.save(categoryExist);
+    }
+
+    const transaction = transactionsRepository.create({
+      title: title,
+      value: value,
+      type: type,
+      category: categoryExist
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
+
+    // if (!categoryExist) {
+    //   const createCategory = categoryRepository.create({
+    //     title: category
+    //   });
+    //
+    //   console.log(createCategory);
+    //
+    //   await categoryRepository.save(createCategory);
+    //   transaction.category = createCategory;
+    //   //transaction.category_id = createCategory.id;
+    // } else {
+    //   //transaction.category_id = categoryExist.id;
+    //   transaction.category = categoryExist;
+    // }
+    //
+    // await transactionsRepository.save(transaction);
+    //
+    // return transaction
   }
 }
 
